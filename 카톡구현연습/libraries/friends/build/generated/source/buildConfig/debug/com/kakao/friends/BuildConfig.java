ad5e351fc1f7b9dd9d32e2642a48196f0e4fc654
chehun-lim <55392535+chehun-lim@users.noreply.github.com>
2019-09-24 01:05:20 +0900

    kakao

---
+++ 카톡구현연습/libraries/friends/build/generated/source/buildConfig/debug/com/kakao/friends/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kakao.friends;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.kakao.friends";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.kakao.friends";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 82;
+  public static final String VERSION_NAME = "1.22.0";
+}
---
+++ 카톡구현연습/libraries/kakaonavi/build/generated/source/buildConfig/androidTest/debug/com/kakao/kakaonavi/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kakao.kakaonavi.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kakao.kakaonavi.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 82;
+  public static final String VERSION_NAME = "1.22.0";
+}
